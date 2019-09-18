d1db102df35196c9256d1d5d74e9e5e8c5848437
Muaviet24 <Muaviet24@gmail.com>
2019-09-18 23:41:30 +0530

    comit Junit

---
+++ src/main/java/com/test/Dummy/Dummy.java
@@ -0,0 +1,12 @@
+package com.test.Dummy;
+
+import org.junit.Test;
+
+public class Dummy {
+	
+	@Test
+	public void test(){
+		System.out.println("Hello");
+	}
+
+}
---
+++ src/main/java/com/test/Dummy/TestRunner.java
@@ -0,0 +1,13 @@
+package com.test.Dummy;
+
+import org.junit.runner.JUnitCore;
+
+public class TestRunner {
+
+	public static void main(String[] args) {
+		
+		JUnitCore.runClasses(Dummy.class);
+
+	}
+
+}
